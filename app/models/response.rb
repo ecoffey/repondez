aa820ef0d141a76894f743a6d0ce89e433f1eb80
Eoin Coffey <ecoffey@gmail.com>
2012-08-12 10:04:01 -0600

    Add validation to rsvping

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,6 @@
 class Response < ActiveRecord::Base
   attr_accessible :attending, :guests, :name, :suggested_song
+
+  validates :name, :presence => true
+  validates :guests, :presence => true, :if => :attending
 end
