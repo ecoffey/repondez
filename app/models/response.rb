ecad69f7871675285f43f9c8ca19d1f4a17cbc22
Eoin Coffey <ecoffey@gmail.com>
2012-08-20 12:11:58 -0600

    Show confirmed guest count

--- app/models/response.rb
+++ app/models/response.rb
@@ -4,4 +4,8 @@ class Response < ActiveRecord::Base
   validates :attending, :presence => true
   validates :name, :presence => true
   validates :guests, :presence => true, :if => :attending
+
+  def self.confirmed_guest_count
+    where(attending: true).sum(:guests)
+  end
 end
