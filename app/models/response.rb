086db9f80c650a90a1417fc57fcf826016766c11
Eoin Coffey <ecoffey@gmail.com>
2012-08-21 22:08:33 -0600

    False is blank, so use inclusion

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ActiveRecord::Base
   attr_accessible :attending, :guests, :name, :suggested_song
 
-  validates :attending, :presence => true
+  validates :attending, :inclusion => { :in => [true, false] }
   validates :name, :presence => true
   validates :guests, :presence => true, :if => :attending
 
